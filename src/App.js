9e58138a5c32d75b687409b17b22dc4c36158f70
Luis Solano <luissolano@iMac-de-Luis-2.local>
2022-03-03 14:48:29 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -32,60 +32,7 @@ function App() {
       setZoom(evt.target.getZoom())
     })
 
-    map.current.on("load", () => {
-      map.current.addSource("covid-cases-source", {
-        type: "geojson",
-        data: `${process.env.REACT_APP_URL_API}/paises`,
-      });
-      map.current.addLayer({
-        id: "covid-cases-layer",
-        type: "circle",
-        source: "covid-cases-source",
-        paint: {
-          "circle-color": "red",
-          "circle-radius": [
-            "step",
-            ["get", "confirmados"],
-            3,100,
-            5,500,
-            7,1000,
-            9,10000,
-            12,50000,
-            15,80000,
-            20,
-          ],
-        },
-      });
-      //agregamos la nueva fuente de datos
-      map.current.addSource("covid-cases-source-poly",{
-        type:"geojson",
-        data:`${process.env.REACT_APP_URL_API}/paises/poly`
-      })
-      //agregamos la simbologia
-      map.current.addLayer({
-        id:  "covid-cases-poly-layer",
-        type:  "fill",
-        source:  "covid-cases-source-poly",
-        paint: {
-          "fill-color": [
-          "step",
-          ["get", "confirmados"],
-          '#FFD456',
-          500,'#FF7733',1000,'#DB5825',10000,'#BC412B',
-          50000,'#A20021',80000,'#A20021']
-        }
-      });
-      
-      map.current.addLayer({
-        id:'covid-cases-layer_text',
-        type:'symbol',
-        source:'covid-cases-source',
-        layout:{
-          "text-field":'{confirmados}',
-          "text-offset":[1,-1]
-        }
-      })
-    });
+    
   });
 
   return (
